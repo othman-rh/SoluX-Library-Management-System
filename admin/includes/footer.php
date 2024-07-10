0224c34754e1d2e06de3c7b5240f04c846d8f63a
othman-rh <144479172+othman-rh@users.noreply.github.com>
2024-07-11 00:47:18 +0100

    Solux

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div>
+    <strong>&copy; 2024 - SoluX Library Management System | Designed and Developed By Othman Rhajbal</strong>
+</footer>
